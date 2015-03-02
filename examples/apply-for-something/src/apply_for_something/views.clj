a28a9047a8e129f5ae6071b773e0df7a74dd49a5
Jim Barritt <jim@planet-ix.com>
2015-03-02 15:32:41 +0000

    [jmdb] Added POST redirect GET to a new application

--- examples/apply-for-something/src/apply_for_something/views.clj
+++ examples/apply-for-something/src/apply_for_something/views.clj
@@ -2,9 +2,13 @@
   (:use [stencil.core]
         [ring.util.response]))
 
+(stencil.loader/set-cache (clojure.core.cache/ttl-cache-factory {} :ttl 0))
+
+(defn uuid [] (str (java.util.UUID/randomUUID)))
+
 (defn get-start-application [request]
   (-> (response (render-file "web-templates/start-application.html" {}))
       (content-type "text/html")))
 
-(defn post-start-application [request]
-  (redirect-after-post "/check-eligibility.html"))
+(defn post-start-application [request]  
+  (redirect-after-post (format "/application-forms/%s/about-you" (uuid))))
