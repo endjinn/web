24dad2ec49dd9442a568645346b24986df54749b
Jim Barritt <jim@planet-ix.com>
2015-03-02 19:34:57 +0000

    [jmdb] Introducing partials

--- examples/apply-for-something/src/apply_for_something/responses.clj
+++ examples/apply-for-something/src/apply_for_something/responses.clj
@@ -5,7 +5,7 @@
 (defn render-html-template-to-response
   ([template] (render-html-template-to-response template {}))
   ([template data]
-   (-> (response (render-file (format "web-templates/%s.html" template) data))
+   (-> (response (render-file (format "templates/%s.html" template) data))
        (content-type "text/html"))))
 
 (defn redirect-to-next-section [section application-id]
