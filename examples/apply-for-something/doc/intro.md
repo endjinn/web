d0da96b2eadf64a5a589e84be2a60cdae9f1d2f6
Jim Barritt <jim@planet-ix.com>
2015-03-02 09:55:27 +0000

    [jmdb] Moving into a proper server project

---
+++ examples/apply-for-something/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to apply-for-something
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ examples/apply-for-something/test/apply_for_something/core_test.clj
@@ -0,0 +1,7 @@
+(ns apply-for-something.core-test
+  (:require [clojure.test :refer :all]
+            [apply-for-something.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
