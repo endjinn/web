c75e60045920dd587767061e706b7366cf2d3359
Jim Barritt <jim@planet-ix.com>
2015-03-03 20:50:54 +0000

    [jmdb] Removing extraneous defroutes from server

--- examples/apply-for-something/src/apply_for_something/server.clj
+++ examples/apply-for-something/src/apply_for_something/server.clj
@@ -15,47 +15,32 @@
 (defroutes app-routes
   (route/resources "/")
 
-  (context "/" []
-           (defroutes root
-             (GET "/" request (redirect "/index.html"))))
+  (context "/" []           
+           (GET "/" request (redirect "/index.html")))
   
-  (context "/start-application" []
-           (defroutes start-application
-             (GET "/" request (v/get-start-application request "01-start-application.html"))
-             (POST "/" request (v/post-start-application request "about-you"))))
+  (context "/start-application" []           
+           (GET "/" request (v/get-start-application request "01-start-application.html"))
+           (POST "/" request (v/post-start-application request "about-you")))
 
   (context "/application-forms/:id/about-you" [id]
-           (defroutes about-you
-             (GET "/" request (v/get-section id request "02-about-you.html"))
-             (POST "/" request (v/post-section id request "where-you-live"))
-             ))
+           (GET "/" request (v/get-section id request "02-about-you.html"))
+           (POST "/" request (v/post-section id request "where-you-live")))
 
-   (context "/application-forms/:id/where-you-live" [id]
-           (defroutes where-you-live
-             (GET "/" request (v/get-section id request "03-where-you-live.html"))
-             (POST "/" request (v/post-section id request "purpose-of-application"))
-             ))
+  (context "/application-forms/:id/where-you-live" [id]
+           (GET "/" request (v/get-section id request "03-where-you-live.html"))
+           (POST "/" request (v/post-section id request "purpose-of-application")))
 
-   (context "/application-forms/:id/purpose-of-application" [id]
-           (defroutes purpose-of-application
-             (GET "/" request (v/get-section id request "04-purpose-of-application.html"))
-             (POST "/" request (v/post-section id request "confirmation"))
-             ))
+   (context "/application-forms/:id/purpose-of-application" [id]           
+           (GET "/" request (v/get-section id request "04-purpose-of-application.html"))
+           (POST "/" request (v/post-section id request "confirmation")))
 
    (context "/application-forms/:id/confirmation" [id]
-           (defroutes confirmation
-             (GET "/" request (v/get-section id request "05-confirmation.html"))
-             (POST "/" request (v/post-section id request "submitted"))
-             ))
+           (GET "/" request (v/get-section id request "05-confirmation.html"))
+           (POST "/" request (v/post-section id request "submitted")))
 
-   (context "/application-forms/:id/submitted" [id]
-           (defroutes submission
-             (GET "/" request (v/get-section id request "06-submitted.html"))             
-             ))
-   
-   
-
-)
+   (context "/application-forms/:id/submitted" [id]           
+           (GET "/" request (v/get-section id request "06-submitted.html")))
+   )
 
 
 
