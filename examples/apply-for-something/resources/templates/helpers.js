f8dfe7914e4b6596adf727414cacab47a75cd5e0
Jim Barritt <jim@planet-ix.com>
2015-03-04 21:45:13 +0000

    [jmdb] Refactored to use a clojure helper instead

--- examples/apply-for-something/resources/templates/helpers.js
+++ examples/apply-for-something/resources/templates/helpers.js
@@ -1,9 +1,9 @@
 Handlebars.registerHelper('stage', function(stageToRender, stageText, options) {
-    var classToAdd = (stageToRender === this.stage) ? "selected" : "";
+    var classToAdd = (stageToRender === this['current-stage']) ? "selected" : "";
     var stageContext = {};
     stageContext.stageClass = classToAdd;
     stageContext.stageText = stageText;
-        
+           
     return new Handlebars.SafeString(options.fn(stageContext));
 });
 
--- examples/apply-for-something/src/apply_for_something/responses.clj
+++ examples/apply-for-something/src/apply_for_something/responses.clj
@@ -6,10 +6,24 @@
 (set-template-path! "/templates" "")
 (register-js-helpers! "resources/templates/helpers.js")
 
+(defn stage-class [stage current-stage]
+  (if (= stage current-stage)
+    "selected"
+    ""))
+
+
+(defhelper progress-stage [ctx options]
+  (let [stage ctx
+        current-stage (.get options "current-stage")
+        stage-context {:stageClass (stage-class stage current-stage)
+                       :stageText (param options 0)}]
+    (safe-str (render-file "progress-stage.html" stage-context))))
+
+
 (defn render-html-template-to-response
   ([template] (render-html-template-to-response template {}))
   ([template model]   
-   (let [data (merge model {:stage template})]
+   (let [data (merge model {:current-stage template})]
      (-> (response (render-file template data))
          (content-type "text/html")))))
 
