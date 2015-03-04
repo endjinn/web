fce84200c987520ebe12efdb5747276a7538abb1
Jim Barritt <jim@planet-ix.com>
2015-03-04 11:22:51 +0000

    [jmdb] Refactored out the sections so that the views functions are generic

--- examples/apply-for-something/src/apply_for_something/views.clj
+++ examples/apply-for-something/src/apply_for_something/views.clj
@@ -9,18 +9,20 @@
   (let [application (a/create-application)]
     (r/redirect-to-next-section next-section (:id application))))
 
-(defn get-section [id request template]
-  (r/render-html-template-to-response template ))
+(def sections
+  {:start-application      ["01-start-application.html" :about-you]
+   :about-you              ["02-about-you.html" :where-you-live]
+   :where-you-live         ["03-where-you-live.html" :purpose-of-application]
+   :purpose-of-application ["04-purpose-of-application.html" :confirmation]
+   :confirmation           ["05-confirmation.html" :submitted]
+   :submitted              ["06-submitted.html"]})
 
-(defn post-section [id request next-section]
-  (r/redirect-to-next-section next-section id))
-
-(def sections-to-templates
-  { :about-you "02-about-you.html"
-   })
-
-(defn get-section-new [id section request]
-  (let [template (section sections-to-templates)]
+(defn get-section [id section request]
+  (let [template (first (section sections))]
     (r/render-html-template-to-response template)))
+
+(defn post-section [id section request]
+  (let [next-section (name (second (section sections)))]   
+    (r/redirect-to-next-section next-section id)))
 
 
