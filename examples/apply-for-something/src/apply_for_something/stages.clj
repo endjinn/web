2361f1a808023e029efb6a3fc499affda5447cd4
Jim Barritt <jim@planet-ix.com>
2015-03-05 16:39:32 +0000

    [jmdb] Refactoring stages into something easier to maintain

---
+++ examples/apply-for-something/src/apply_for_something/stages.clj
@@ -0,0 +1,27 @@
+(ns apply-for-something.stages)
+
+(defn stage-entry [input, next-stage]
+  (let [stage (first input)
+        template (second input)
+        stage-data (if (nil? next-stage) [template] [template next-stage])]
+    [(first input) stage-data]))
+
+(defn index-of [item list]
+  (first (keep-indexed #(if (= item %2) %1) list)))
+
+(defn next-item [list item]
+  (let [item-idx (index-of item list)
+        last-idx (dec (count list))]
+    (if (< item-idx last-idx)      
+      (get list (inc item-idx)))))
+
+(defn configure-stage [stages-list result stage]
+  (let [next-item (first (next-item stages-list stage))] 
+    (apply conj result  (stage-entry stage next-item))))
+
+(defn configure-stages [stages-list]
+  (let [data (reduce (partial configure-stage stages-list) [] stages-list)]
+    (apply array-map data)))
+
+(defn defstages [stages-list]
+  (def ^:dynamic *stages* (configure-stages stages-list)))
--- examples/apply-for-something/src/apply_for_something/views.clj
+++ examples/apply-for-something/src/apply_for_something/views.clj
@@ -1,29 +1,31 @@
-(ns apply-for-something.views  
+(ns apply-for-something.views
+  (:use [apply-for-something.stages])
   (:require [apply-for-something.application :as a]
             [apply-for-something.responses :as r]))
 
-(def sections
-  {:start-application      ["01-start-application.html" :about-you]
-   :about-you              ["02-about-you.html" :where-you-live]
-   :where-you-live         ["03-where-you-live.html" :purpose-of-application]
-   :purpose-of-application ["04-purpose-of-application.html" :confirmation]
-   :confirmation           ["05-confirmation.html" :submitted]
-   :submitted              ["06-submitted.html"]})
+
+(defstages
+  [[:start-application      "01-start-application.html"]
+   [:about-you              "02-about-you.html"]
+   [:where-you-live         "03-where-you-live.html"]
+   [:purpose-of-application "04-purpose-of-application.html"]
+   [:confirmation           "05-confirmation.html"]
+   [:submitted              "06-submitted.html"]])
 
 (defn get-start-application [request]
-  (r/render-html-template-to-response (first (:start-application sections))))
+  (r/render-html-template-to-response (first (:start-application *stages*))))
 
 (defn post-start-application [request]
   (let [application (a/create-application)]
-    (r/redirect-to-next-section (name (second (:start-application sections))) (:id application))))
+    (r/redirect-to-next-section (name (second (:start-application *stages*))) (:id application))))
 
 
 (defn get-section [id section request]
-  (let [template (first (section sections))]
+  (let [template (first (section *stages*))]
     (r/render-html-template-to-response template)))
 
 (defn post-section [id section request]
-  (let [next-section (name (second (section sections)))]   
+  (let [next-section (name (second (section *stages*)))]   
     (r/redirect-to-next-section next-section id)))
 
 
